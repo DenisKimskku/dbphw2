4a03dec89e0009b0c619818ffd154e518f76ed2c
DenisKimskku <kor8821@naver.com>
2023-10-11 14:50:03 +0900

    p1-1 solved with pics and 1-2 mapper and reducer v1.2

--- MapReduceCodes/mapper.py
+++ MapReduceCodes/mapper.py
@@ -5,7 +5,7 @@ import sys
 for line in sys.stdin:
     ########## EDIT HERE ##########
     #mapper for reducer.py
-    line = line.strip().split()
+    line = line.strip().split(',')
     print("{0} {1} {2} {3} {4}".format(line[0], line[1], line[2], line[3], line[4]))
 
     ########## EDIT HERE ##########
