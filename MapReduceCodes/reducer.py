24e2e12c24136ec2d86c30f26134a5094b771782
DenisKimskku <kor8821@naver.com>
2023-10-11 14:25:41 +0900

    p1-1 solved with pics and 1-2 mapper and reducer v1.1

--- MapReduceCodes/reducer.py
+++ MapReduceCodes/reducer.py
@@ -8,7 +8,31 @@ for line in sys.stdin:
     lines.append(line)
 
 ########## EDIT HERE ##########
+#find pareto optimal points
+#maximize quality and service, minimize price
+#do not use brute force
+#only choose the city 'Suwon'
+#this is reducer, mapper is mapper.py
 
+skyline = []
+not_skyline = []
+
+for id_a, city_a, quality_a, service_a, price_a in lines:
+    if(city_a != 'Suwon'):
+        continue
+    dominated = False
+    for id_b, city_b, quality_b, service_b, price_b in lines:
+        if id_a == id_b:
+            continue
+        # Checks whether a is dominated by b
+        # quality feature should be maximized and price feature should be minimized
+        if (int(quality_a) <= int(quality_b) and int(service_a) <= int(service_b) and int(price_a) >= int(price_b)) and (int(quality_a) < int(quality_b) or int(service_a) < int(service_b) or int(price_a) > int(price_b)):
+            dominated = True
+            break
+    if not dominated:
+        skyline.append((id_a, city_a, quality_a, service_a, price_a))
+    else:
+        not_skyline.append((id_a, city_a, quality_a, service_a, price_a))
 
 
 
